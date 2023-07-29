f9e3113075d4a3c5e615508510bf1890b3b35aa7
Edio Gama <ediogama@hotmail.com>
2023-07-28 21:01:11 -0300

    add createCategory

---
+++ internal/service/category.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"context"
+
+	"github.com/ediogama/gRPC-golang/internal/database"
+	"github.com/ediogama/gRPC-golang/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, request *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.CreateCategory(request.Name, request.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, nil
+}
