db7a06619a8c70cd215cc968e0f86d42e823d079
Edio Gama <ediogama@hotmail.com>
2023-07-29 15:19:34 -0300

    add stream CreateCategory return List

--- internal/service/category.go
+++ internal/service/category.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/ediogama/gRPC-golang/internal/database"
 	"github.com/ediogama/gRPC-golang/internal/pb"
@@ -64,4 +65,29 @@ func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryID) (*
 	}
 
 	return categoryResponse, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.CreateCategory(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+	}
 }
